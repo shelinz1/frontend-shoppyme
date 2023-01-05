8c91594e84e3d1957bfe7786abba519a8821a9ae
shadrach i <itodoshadrach@gmail.com>
2023-01-05 13:59:03 +0100

    more 👩‍💻

--- src/screens/placeOrderScreen/PlaceOrderScreen.jsx
+++ src/screens/placeOrderScreen/PlaceOrderScreen.jsx
@@ -51,12 +51,12 @@ function PlaceOrderScreen() {
     navigate("/");
   };
 
-  // useEffect(() => {
-  //   if (success) {
-  //     navigate(`/order/${order._id}`);
-  //     dispatch({ type: CREATE_ORDER_RESET });
-  //   }
-  // }, [success, dispatch, navigate, order]);
+  useEffect(() => {
+    if (success) {
+      navigate(`/order/${order._id}`);
+      dispatch({ type: CREATE_ORDER_RESET });
+    }
+  }, [success, dispatch, navigate, order]);
 
   const placeOrderHandler = (e) => {
     e.preventDefault();
@@ -71,7 +71,6 @@ function PlaceOrderScreen() {
         totalPrice: cart.totalPrice,
       })
     );
-    navigate(`/order/${order._id}`);
   };
 
   return (
