0a1e77e535941f62f59110858283ba24976471cf
shadrach i <itodoshadrach@gmail.com>
2023-01-05 10:41:30 +0100

    made changes to place order screen

--- src/screens/placeOrderScreen/PlaceOrderScreen.jsx
+++ src/screens/placeOrderScreen/PlaceOrderScreen.jsx
@@ -38,10 +38,12 @@ function PlaceOrderScreen() {
     cartItems.reduce((total, value) => total + value.quantity * value.price, 0)
   );
 
-  cart.shippingPrice =
-    cart.productsPrice > 100 ? calculatedDecimal(0) : calculatedDecimal(10);
+  cart.shippingPrice = calculatedDecimal(cart.productsPrice > 100 ? 0 : 10);
+  // cart.productsPrice > 100 ? calculatedDecimal(0) : calculatedDecimal(10);
 
-  cart.taxPrice = calculatedDecimal(0.15 * cart.productsPrice);
+  cart.taxPrice = calculatedDecimal(
+    Number(0.15 * cart.productsPrice).toFixed()
+  );
 
   cart.totalPrice = (
     Number(cart.productsPrice) +
@@ -53,12 +55,12 @@ function PlaceOrderScreen() {
     navigate("/");
   };
 
-  useEffect(() => {
-    if (success) {
-      navigate(`/order/${order._id}`);
-      dispatch({ type: CREATE_ORDER_RESET });
-    }
-  }, [success, dispatch, navigate, order]);
+  // useEffect(() => {
+  //   if (success) {
+  //     navigate(`/order/${order._id}`);
+  //     dispatch({ type: CREATE_ORDER_RESET });
+  //   }
+  // }, [success, dispatch, navigate, order]);
 
   const placeOrderHandler = (e) => {
     e.preventDefault();
@@ -73,6 +75,7 @@ function PlaceOrderScreen() {
         totalPrice,
       })
     );
+    navigate(`/order/${order._id}`);
   };
 
   return (
@@ -200,22 +203,22 @@ function PlaceOrderScreen() {
             <tbody>
               <tr>
                 <td>Products price</td>
-                <td>$ {productsPrice}</td>
+                <td>${productsPrice}</td>
               </tr>
               <tr>
                 <td>Shipping Price</td>
-                <td>$ {shippingPrice}</td>
+                <td>${shippingPrice}</td>
               </tr>
 
               <tr>
                 <td>Tax price</td>
-                <td>$ {taxPrice}</td>
+                <td>${taxPrice}</td>
               </tr>
 
               <tr>
                 <td>Total price</td>
                 <td>
-                  <strong>$ {totalPrice}</strong>
+                  <strong>${totalPrice}</strong>
                 </td>
               </tr>
             </tbody>
@@ -258,3 +261,199 @@ function PlaceOrderScreen() {
 }
 
 export default PlaceOrderScreen;
+
+// import Axios from 'axios';
+// import React, { useContext, useEffect, useReducer } from 'react';
+// import { Helmet } from 'react-helmet-async';
+// import { Link, useNavigate } from 'react-router-dom';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
+// import ListGroup from 'react-bootstrap/ListGroup';
+// import { toast } from 'react-toastify';
+// import { getError } from '../utils';
+// import { Store } from '../Store';
+// import CheckoutSteps from '../components/CheckoutSteps';
+// import LoadingBox from '../components/LoadingBox';
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'CREATE_REQUEST':
+//       return { ...state, loading: true };
+//     case 'CREATE_SUCCESS':
+//       return { ...state, loading: false };
+//     case 'CREATE_FAIL':
+//       return { ...state, loading: false };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default function PlaceOrderScreen() {
+//   const navigate = useNavigate();
+
+//   const [{ loading }, dispatch] = useReducer(reducer, {
+//     loading: false,
+//   });
+
+//   const { state, dispatch: ctxDispatch } = useContext(Store);
+//   const { cart, userInfo } = state;
+
+//   const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100; // 123.2345 => 123.23
+//   cart.itemsPrice = round2(
+//     cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+//   );
+//   cart.shippingPrice = cart.itemsPrice > 100 ? round2(0) : round2(10);
+//   cart.taxPrice = round2(0.15 * cart.itemsPrice);
+//   cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+
+//   const placeOrderHandler = async () => {
+//     try {
+//       dispatch({ type: 'CREATE_REQUEST' });
+
+//       const { data } = await Axios.post(
+//         '/api/orders',
+//         {
+//           orderItems: cart.cartItems,
+//           shippingAddress: cart.shippingAddress,
+//           paymentMethod: cart.paymentMethod,
+//           itemsPrice: cart.itemsPrice,
+//           shippingPrice: cart.shippingPrice,
+//           taxPrice: cart.taxPrice,
+//           totalPrice: cart.totalPrice,
+//         },
+//         {
+//           headers: {
+//             authorization: `Bearer ${userInfo.token}`,
+//           },
+//         }
+//       );
+//       ctxDispatch({ type: 'CART_CLEAR' });
+//       dispatch({ type: 'CREATE_SUCCESS' });
+//       localStorage.removeItem('cartItems');
+//       navigate(`/order/${data.order._id}`);
+//     } catch (err) {
+//       dispatch({ type: 'CREATE_FAIL' });
+//       toast.error(getError(err));
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (!cart.paymentMethod) {
+//       navigate('/payment');
+//     }
+//   }, [cart, navigate]);
+
+//   return (
+//     <div>
+//       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
+//       <Helmet>
+//         <title>Preview Order</title>
+//       </Helmet>
+//       <h1 className="my-3">Preview Order</h1>
+//       <Row>
+//         <Col md={8}>
+//           <Card className="mb-3">
+//             <Card.Body>
+//               <Card.Title>Shipping</Card.Title>
+//               <Card.Text>
+//                 <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
+//                 <strong>Address: </strong> {cart.shippingAddress.address},
+//                 {cart.shippingAddress.city}, {cart.shippingAddress.postalCode},
+//                 {cart.shippingAddress.country}
+//               </Card.Text>
+//               <Link to="/shipping">Edit</Link>
+//             </Card.Body>
+//           </Card>
+
+//           <Card className="mb-3">
+//             <Card.Body>
+//               <Card.Title>Payment</Card.Title>
+//               <Card.Text>
+//                 <strong>Method:</strong> {cart.paymentMethod}
+//               </Card.Text>
+//               <Link to="/payment">Edit</Link>
+//             </Card.Body>
+//           </Card>
+
+//           <Card className="mb-3">
+//             <Card.Body>
+//               <Card.Title>Items</Card.Title>
+//               <ListGroup variant="flush">
+//                 {cart.cartItems.map((item) => (
+//                   <ListGroup.Item key={item._id}>
+//                     <Row className="align-items-center">
+//                       <Col md={6}>
+//                         <img
+//                           src={item.image}
+//                           alt={item.name}
+//                           className="img-fluid rounded img-thumbnail"
+//                         ></img>{' '}
+//                         <Link to={`/product/${item.slug}`}>{item.name}</Link>
+//                       </Col>
+//                       <Col md={3}>
+//                         <span>{item.quantity}</span>
+//                       </Col>
+//                       <Col md={3}>${item.price}</Col>
+//                     </Row>
+//                   </ListGroup.Item>
+//                 ))}
+//               </ListGroup>
+//               <Link to="/cart">Edit</Link>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//         <Col md={4}>
+//           <Card>
+//             <Card.Body>
+//               <Card.Title>Order Summary</Card.Title>
+//               <ListGroup variant="flush">
+//                 <ListGroup.Item>
+//                   <Row>
+//                     <Col>Items</Col>
+//                     <Col>${cart.itemsPrice.toFixed(2)}</Col>
+//                   </Row>
+//                 </ListGroup.Item>
+//                 <ListGroup.Item>
+//                   <Row>
+//                     <Col>Shipping</Col>
+//                     <Col>${cart.shippingPrice.toFixed(2)}</Col>
+//                   </Row>
+//                 </ListGroup.Item>
+//                 <ListGroup.Item>
+//                   <Row>
+//                     <Col>Tax</Col>
+//                     <Col>${cart.taxPrice.toFixed(2)}</Col>
+//                   </Row>
+//                 </ListGroup.Item>
+//                 <ListGroup.Item>
+//                   <Row>
+//                     <Col>
+//                       <strong> Order Total</strong>
+//                     </Col>
+//                     <Col>
+//                       <strong>${cart.totalPrice.toFixed(2)}</strong>
+//                     </Col>
+//                   </Row>
+//                 </ListGroup.Item>
+//                 <ListGroup.Item>
+//                   <div className="d-grid">
+//                     <Button
+//                       type="button"
+//                       onClick={placeOrderHandler}
+//                       disabled={cart.cartItems.length === 0}
+//                     >
+//                       Place Order
+//                     </Button>
+//                   </div>
+//                   {loading && <LoadingBox></LoadingBox>}
+//                 </ListGroup.Item>
+//               </ListGroup>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//       </Row>
+//     </div>
+//   );
+// }
